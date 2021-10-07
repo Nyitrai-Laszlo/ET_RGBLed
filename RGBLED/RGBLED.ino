5f85e8db3fd081ee6fae883bfb19332b303e7ed3
Laszlo <nyitrai.laszlo01@gmail.com>
2021-10-07 12:43:46 +0200

    Led teszt

--- RGBLED/RGBLED.ino
+++ RGBLED/RGBLED.ino
@@ -3,11 +3,24 @@ int b=10;
 int g=11;
 
 void setup() {
-  // put your setup code here, to run once:
+ pinMode(r, OUTPUT);
+ pinMode(b, OUTPUT);
+ pinMode(g, OUTPUT);
 
+ digitalWrite(r,1); delay(500);
+
+ digitalWrite(r,0);
+digitalWrite(b,1); delay(500);
+
+digitalWrite(b,0);
+digitalWrite(g,1); delay(500);
+
+digitalWrite(r,1);
+digitalWrite(b,1); delay(500);
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+ 
 
 }
