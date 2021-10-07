73a8e2c739de866caa1b03b1b494c048c3812b0c
Laszlo <nyitrai.laszlo01@gmail.com>
2021-10-07 14:25:34 +0200

    Led teszt

--- RGBLED/RGBLED.ino
+++ RGBLED/RGBLED.ino
@@ -2,13 +2,22 @@ int r=9;
 int b=10;
 int g=11;
 
+byte fenyEro=0;
+byte irany=1;
+
+int poti=0;
+
+
 void setup() {
  pinMode(r, OUTPUT);
  pinMode(b, OUTPUT);
- pinMode(g, OUTPUT);
-
- digitalWrite(r,1); delay(500);
-
+ pinMode(g, OUTPUT); 
+pinMode(A0, INPUT); Serial.begin(9600); 
+pinMode(A1,INPUT);
+  pinMode(A2,INPUT);
+  
+ digitalWrite(r,1); delay(500); 
+ 
  digitalWrite(r,0);
 digitalWrite(b,1); delay(500);
 
@@ -17,10 +26,46 @@ digitalWrite(g,1); delay(500);
 
 digitalWrite(r,1);
 digitalWrite(b,1); delay(500);
+
+digitalWrite(r,0);
+digitalWrite(g,0);
+digitalWrite(b,0);
+
+//analogWrite(r,125);
+//analogWrite(g,15);
+//analogWrite(b,214);
+
 }
 
 
 void loop() {
  
+
+poti=analogRead(A0);
+fenyEro=map (poti,0,1023,0,255);
+analogWrite(r,fenyEro);
+
+poti=analogRead(A1);
+fenyEro=map (poti,0,1023,0,255);
+analogWrite(g,fenyEro);
+
+poti=analogRead(A2);
+fenyEro=map (poti,0,1023,0,255);
+analogWrite(b,fenyEro);
+
+  /*Serial.print("Analóg érték: ");
+  Serial.print(poti_3);
+  Serial.print("; Fényerő: ");
+  Serial.println(fenyEro);*/
+  
+
+    
+  //analogWrite(r,fenyEro);
+ //analogWrite(b,fenyEro);
+ //analogWrite(g,fenyEro);
+ /*if (fenyEro==255) irany=-1;
+ if (fenyEro==0) irany=1;
+ fenyEro=fenyEro+irany;
+ delay(10);*/
 
 }
